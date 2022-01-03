6379c06f36ecf869e9603a7ef876fc320199395f
bcerquiare <bcerquiare@outlook.com>
2022-01-03 08:26:51 -0300

    exports from public-api

--- projects/ngx-cerquiare-modal/src/public-api.ts
+++ projects/ngx-cerquiare-modal/src/public-api.ts
@@ -3,5 +3,15 @@
  */
 
 export * from './lib/ngx-cerquiare-modal.service';
-export * from './lib/ngx-cerquiare-modal.component';
 export * from './lib/ngx-cerquiare-modal.module';
+
+export * from './lib/components/ngx-cerquiare-modal-backdrop/ngx-cerquiare-modal-backdrop.component';
+export * from './lib/components/window/ngx-cerquiare-modal-window/ngx-cerquiare-modal-window.component'
+
+export * from './lib/interfaces/INgxCerquiareModalConfig'
+export * from './lib/interfaces/INgxCerquiareModalWindowComponent'
+
+export * from './lib/enum/ENgxCerquiareModalWindowPosition'
+export * from './lib/enum/ENgxCerquiareModalWindowType'
+
+export * from './lib/classes/NgxCerquiareModalComponentBase'
